092450632c2404d92f2c481e2083f590ad57ac89
markwuebbens <mwuebben@andrew.cmu.edu>
2018-02-26 01:39:25 -0500

    logic updates to arbitrate_network_conditions

--- network.py
+++ network.py
@@ -72,83 +72,75 @@ class DSRC_Network:
     def arbitrate_channel_conditions(self):
 
         tx_sorted = sorted(self.tx_nodes, key=lambda node: node.x)
-        tx_locations = [node.x for node in tx_sorted]
-        len_txers = len(tx_locations)
+        len_txers = len(tx_sorted)
 
         all_sorted = sorted(self.all_nodes, key=lambda node: node.x)
-
-        #Build up a string of 'complete packet's which were received this round
-        message_aggr = ""
+        len_all = len(all_sorted)
 
         #FIXME - probably put elsewhere
-        def _in_range(node, tx_node):
-            return abs(node.x - tx_node.x) <= TX_RANGE
+        def _in_range(node, other):
+            return abs(node.x - other.x) <= TX_RANGE
 
+        def _below_range(node, other):
+            return (other.x < node.x) and (other.x + TX_RANGE >= node.x)
 
+        """
+        def _above_range(node, other):
+            return (node.x < other.x) and (node.x + TX_RANGE >= other.x)
+        """
         #This index variable walks through the sorted list of tx'ing nodes
         #index always points to the lowest relevant tx node to consider
-        index = 0
+        lo_tx_i = 0
 
         #Determine state of channel for each node
-        for node in all_sorted:
+        for (this_i, this_node) in enumerate(all_sorted):
 
-            this_i = index
-            lo_index = index
-            hi_index = index
-            lo_node = None
-            hi_node = None
+            #Update lo_tx_i (closest txer strictly behind this node)
+            while (_below_range(this_node, tx_sorted[lo_tx_i])):
 
+                if (lo_tx_i + 1 < len_txers) and
+                   (_below_range(this_node, tx_sorted[lo_tx_i + 1])):
 
-            #Find closest txer behind this node
-            while (this_i < len_txers):
-                if (tx_locations[this_i] > node.x):
-                    #That tx'er isn't behind us!
-                    break
+                    lo_tx_i += 1
                 else:
-                    lo_node = tx_sorted[this_i]
-                    lo_index = this_i
-                    this_i += 1
+                    break
 
-            #Find txer in front of this node
-            if (this_i < len_txers):
-                hi_node = tx_sorted[this_i]
-                hi_index = this_i
+            #Find lo_i and hi_i
+            # (furthest nodes within range behind and ahead of this one)
+            lo_i = this_i
+            while (lo_i - 1 >= 0) and
+                  (_in_range(this_node, all_sorted[lo_i - 1])):
+                lo_i -= 1
 
-            #Deliver if applicable
-            if (hi_node and not lo_node):
-                if (_in_range(node, hi_node)):
+            hi_i = this_i
+            while (hi_i + 1 < len_all) and
+                  (_in_range(this_node, all_sorted[hi_i + 1])):
+                hi_i += 1
 
-                    if not (hi_index + 1 < len_txers):
-                        #No other txer in front of this one
-                        node.delivery_from(hi_node)
+            #Determine if a valid tx'er node is in range
+            # (Deliver the message piece to this_node if so)
+            if (lo_tx_i + 1 < len_txers) and
+               (_in_range(this_node, tx_sorted[lo_tx_i + 1]) and
+               (not _in_range(this_node, tx_sorted[lo_tx_i])):
 
-                    elif not _in_range(node, tx_sorted[hi_index + 1]):
-                        #Next txer is out of range
-                        node.delivery_from(hi_node)
+                this_node.delivery_from(tx_sorted[lo_tx_i + 1])
 
-            elif (lo_node and not hi_node):
-                if (_in_range(node, lo_node)):
+            elif (_in_range(tx_sorted[lo_tx_i])):
 
-                    if not (lo_index - 1 >= 0):
-                        #No other txer behind this one
-                        node.delivery_from(lo_node)
+                this_node.delivery_from(tx_sorted[lo_tx_i])
 
-                    elif not _in_range(node, tx_sorted[lo_index - 1]):
-                        #Next txer is out of range
-                        node.delivery_from(lo_node)
-
-            #Update node's channel status
-            if (lo_node is not None and _in_range(node, lo_node)):
+            #Update this_node's internal understanding of the world
+            if (lo_tx_node is not None and _in_range(node, lo_tx_node)):
                 node.channel_is_idle = False
 
-            elif (hi_node is not None and _in_range(node, hi_node)):
+            elif (hi_tx_node is not None and _in_range(node, hi_tx_node)):
                 node.channel_is_idle = False
 
             else:
                 node.channel_is_idle = True
 
             #Move index up to new lowest tx'er
-            index = lo_index
+            index = lo_tx
 
         #FIXME - here
         return message_aggr
