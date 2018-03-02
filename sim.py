3aba1f1d6a4d6c85cab4a2bc5e12358a71df05d7
markwuebbens <mwuebben@andrew.cmu.edu>
2018-03-02 09:37:22 -0500

    Commits top level sim changes to match node and network changes from last commit

--- sim.py
+++ sim.py
@@ -7,9 +7,6 @@ from node import DSRC_Node, State
 
 from config import *
 
-#from network import DSRC_Network
-#from node import DSRC_Node
-
 node_id = 0
 
 class Clock():
@@ -54,39 +51,36 @@ def main():
 
     num_finished = 0
 
-    print_intro(clock)
-
     while(1):
-
-        if (clock.time > END_TIME):
-            #network.compute_and_display_final_stats()
-            print_summary(clock, this_network, num_finished)
-            exit(1)
-
-        clock.tick()
 
         #generate and add new nodes
         generate_node(clock, this_network)
 
+        #Execute one logical step of the simulation
+        # -Each node: Moves forward logically in time and space
+        # -'Finished' nodes are removed from the network and returned
+        finished_nodes = this_network.step_sim()
+        num_finished += len(finished_nodes)
+
         #Network arbitrates message tx'ing and collision for curr state
-        # (First sorts tx_nodes and all_nodes lists sorted here)
-        # (Each node: Checks immediate for valid transmission)
-        # (           Logs any successfull full packets recv'd)
-        # (           Saves the local channel state)
+        # -First sorts tx_nodes and all_nodes lists sorted here
+        # -Each node: Receives any valid transmission
+        #             Receives it's current local channel state
         #
-        # MUST BE CALLED BEFORE network.step_sim()!
+        # MUST BE CALLED BEFORE network.transition_sim()!
         this_network.arbitrate_channel_conditions()
 
         #Network elements increment logically in time and space
-        # (Each node: Executes (cs -> ns) in the DSRC FSM
-        # (           Moves forward logically in time and space)
-        # 'Finished' nodes are removed from the network and returned
-        (finished_nodes, finished_msgs) = this_network.step_sim()
+        # -Each node: Executes (cs -> ns) in the DSRC FSM according to local
+        #   conditions
+        this_network.transition_sim()
 
-        num_finished += len(finished_nodes)
+        #Finish at some point...
+        if (clock.time > END_TIME):
+            exit(1)
 
-        if finished_msgs:
-            print "> {:.6f}s\n".format(clock.time), finished_msgs,
+        clock.tick()
+
 
 main()
 
